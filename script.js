0e5eee044f43f66185ac13fa9456a2cee0b47c90
rapaah <mraffa.yachyach@gmail.com>
2025-11-05 12:31:17 +0700

    Start to play with responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+const burgerIcon = document.getElementById("burgerIcon");
+
+burgerIcon.addEventListener("click", () => {
+    alert("sabar lu, belum jadi")
+})
+
+window.addEventListener('load', function() {
+        alert("This web maybe will a bit lagging in some device, try from PC for best experience.");
+        document.body.classList.add('start-animation'); 
+    });
