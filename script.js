a0d6981f99c8c92481ca6909b9b75027e6ed6099
rapaah <mraffa.yachyach@gmail.com>
2025-11-05 21:29:14 +0700

    Responsive on click NavBar

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const burgerIcon = document.getElementById("burgerIcon");
-
-burgerIcon.addEventListener("click", () => {
-    alert("sabar lu, belum jadi")
-})
+const navList = document.getElementById("menu-list")
 
 window.addEventListener('load', function() {
         alert("This web maybe will a bit lagging in some device, try from PC for best experience.");
         document.body.classList.add('start-animation'); 
     });
+
+
+burgerIcon.addEventListener("click", () => {
+    navList.classList.toggle("hidden");
+})
