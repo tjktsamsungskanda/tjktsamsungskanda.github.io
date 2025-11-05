853687b6da65a5691c0da196495e35106b5680b4
rapaah <mraffa.yachyach@gmail.com>
2025-11-05 21:42:44 +0700

    a bit repair on script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const burgerIcon = document.getElementById("burgerIcon");
 const navList = document.getElementById("menu-list")
 
-window.addEventListener('load', function() {
-        alert("This web maybe will a bit lagging in some device, try from PC for best experience.");
-        document.body.classList.add('start-animation'); 
-    });
-
+document.addEventListener('DOMContentLoaded', function() {
+    alert("This web maybe will a bit lagging in some device, try from PC for best experience.");
+    document.body.classList.add('start-animation'); 
+});
 
 burgerIcon.addEventListener("click", () => {
     navList.classList.toggle("hidden");
